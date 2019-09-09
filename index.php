7c2ce54c6803144f250033c6736e3cef3e63b42e
enrico76 <enricodip@gmail.com>
2019-09-09 18:04:23 +0200

    create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>php-ajax-dischi</title>
+    <?php
+      $prova = "ciao";
+
+      var_dump($prova);
+
+
+     ?>
+  </head>
+  <body>
+
+  </body>
+</html>
